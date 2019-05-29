dda4a6177238e0e4a9fa7d60b68c9e1b28cf7f58
Gayathiri <gselvam@ztech.io>
2019-05-29 08:37:22 +0530

    Modified shopping controller.

--- src/controllers/shopping-controller.ts
+++ src/controllers/shopping-controller.ts
@@ -17,26 +17,52 @@ export class ShoppingController {
 
 
 
-    @Get('/shopSearch?:text')
+    @Get('/shopResults/:text')
     async searchShops(@Req() req: any, @Res() res: any, @Body() body: any): Promise<any> {
         try {
             let latitude = req.headers.latitude;
             let longitude = req.headers.longitude;
-            let queryString = req.query.text;
-            let requestURL = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${queryString}&inputtype=textquery&fields=photos,place_id,types,formatted_address,name,opening_hours,rating,price_level&locationbias=point:${latitude},${longitude}&key=AIzaSyCCGb_0fod9vxW27A5iuYwNbW_x2JiCAvc`;
-            let response = await this.googleMapsAPI.fetchData(requestURL);
-            let result = this.retrieveShopSearch(response);
-            return this.responseUtility.generateResponse(true, result);
+            let queryString = req.params.text;
+            let requestURL = `https://maps.googleapis.com/maps/api/place/textsearch/json?key=AIzaSyCCGb_0fod9vxW27A5iuYwNbW_x2JiCAvc&query=${queryString}`;
+            let response: any = await this.googleMapsAPI.fetchData(requestURL);
+            let token = response.next_page_token;
+            let result = this.retrieveShopResults(response);
+            return this.responseUtility.generateResponse(true, { results: result, token: token });
         } catch (err) {
             return { isSuccess: false };
         }
 
     }
 
+
+    private retrieveShopResults(response: any) {
+        let results: Shop[] = new Array<Shop>();;
+        let candidates = response.results;
+        candidates.forEach((candidate: any) => {
+                let shop = new Shop();
+                shop.name = candidate.name;
+                shop.address = candidate.formatted_address;
+                shop.openNow = candidate.opening_hours ? candidate.opening_hours.open_now : 'NA';
+                shop.placeId = candidate.place_id;
+                shop.rating = candidate.rating;
+                shop.types = candidate.types;
+                shop.latitude = candidate.geometry.lat;
+                shop.longitude = candidate.geometry.lng;
+                if (candidate.photos && candidate.photos.length > 0) {
+                    shop.photoReference = candidate.photos[0].photo_reference;
+                    shop.imageLink = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${shop.photoReference}&key=AIzaSyCCGb_0fod9vxW27A5iuYwNbW_x2JiCAvc`;
+                }
+                results.push(shop);
+        });
+
+        return results;
+
+    }
+
     private retrieveShopSearch(response: any) {
         let result: Shop[] = new Array<Shop>();;
         let candidates = response.candidates;
-        if (candidates.length > 0 && (candidates[0].types.indexOf('store') != -1 || candidates[0].types.indexOf('shopping_mall') != -1 )) {
+        if (candidates.length > 0 && (candidates[0].types.indexOf('store') != -1 || candidates[0].types.indexOf('shopping_mall') != -1)) {
             let candidate = candidates[0];
             let shop = new Shop();
             shop.name = candidate.name;
--- src/server.ts
+++ src/server.ts
@@ -23,6 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 /**
  * Morgan logging Config
